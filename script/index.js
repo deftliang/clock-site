264f8e27708e9b24f19391937c7233d3ff4db325
deft <deftmail@163.com>
2020-09-13 15:34:13 +0800

    master

--- script/index.js
+++ script/index.js
@@ -1,8 +1,36 @@
 $(document).ready(function () {
+    /**
+     * 显示模式开关
+     */
+    let flag = false
+
+    /**
+     * 时钟运行
+     */
     refresh()
     setInterval(() => {
         refresh()
     }, 1000)
+
+    /**
+     * 点击body切换显示模式
+     */
+    $('body').on('click', () => {
+        flag = !flag
+        if (flag) {
+            $('.clock').html(`
+            <span class="hours"></span
+            >&nbsp;:&nbsp;<span class="minutes"></span
+            >
+            `)
+        } else {
+            $('.clock').html(`
+            <span class="hours"></span
+            >&nbsp;:&nbsp;<span class="minutes"></span
+            >&nbsp;:&nbsp;<span class="seconds"></span>
+            `)
+        }
+    })
 
     function refresh() {
         /**
@@ -13,8 +41,8 @@ $(document).ready(function () {
         const getMinutes = dt.getMinutes()
         const getSeconds = dt.getSeconds()
         /**
-        * 补足两位数
-        */
+         * 补足两位数
+         */
         const h = getHours < 10 ? `0${getHours}` : `${getHours}`
         const m = getMinutes < 10 ? `0${getMinutes}` : `${getMinutes}`
         const s = getSeconds < 10 ? `0${getSeconds}` : `${getSeconds}`
