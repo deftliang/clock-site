f2e14006f30f81e29215cfd3e35b83ffc79d00c8
deft <deftmail@163.com>
2020-09-13 15:01:21 +0800

    master

--- script/index.js
+++ script/index.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
     }, 1000)
 
     function refresh() {
-        let dt = new Date()
-        let h = dt.getHours() < 10 ? `0${dt.getHours()}` : `${dt.getHours()}`
-        let m = dt.getMinutes() < 10 ? `0${dt.getMinutes()}` : `${dt.getMinutes()}`
-        let s = dt.getSeconds() < 10 ? `0${dt.getSeconds()}` : `${dt.getSeconds()}`
-        // console.log(dt, h, m, s)
+        const dt = new Date()
+        const getHours = dt.getHours()
+        const getMinutes = dt.getMinutes()
+        const getSeconds = dt.getSeconds()
+        const h = getHours < 10 ? `0${getHours}` : `${getHours}`
+        const m = getMinutes < 10 ? `0${getMinutes}` : `${getMinutes}`
+        const s = getSeconds < 10 ? `0${getSeconds}` : `${getSeconds}`
         $('.hours').text(h)
         $('.minutes').text(m)
         $('.seconds').text(s)
